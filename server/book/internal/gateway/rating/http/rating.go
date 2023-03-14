0a94368cf3f9f232f0ea3132bca9e74b016891f3
basebandit <evansonmwangi83@gmail.com>
2023-03-14 11:11:59 +0300

    refactor to use the service discovery registry

--- server/book/internal/gateway/rating/http/rating.go
+++ server/book/internal/gateway/rating/http/rating.go
@@ -4,25 +4,32 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 
 	"github.com/basebandit/bookshop/server/book/internal/gateway"
+	"github.com/basebandit/bookshop/server/pkg/discovery"
 	"github.com/basebandit/bookshop/server/rating/pkg/model"
 )
 
 // Gateway defines an HTTP gateway for a rating service.
 type Gateway struct {
-	addr string
+	registry discovery.Registry
 }
 
 // New creates a new HTTP gateway for a rating service
-func New(addr string) *Gateway {
-	return &Gateway{addr}
+func New(registry discovery.Registry) *Gateway {
+	return &Gateway{registry}
 }
 
 // GetAggregatedRating returns the aggregated rating for a record or ErrNotFound if there are no ratings for it.
 func (g *Gateway) GetAggregatedRating(ctx context.Context, recordID model.RecordID, recordType model.RecordType) (float64, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%v/rating", g.addr), nil)
+	addrs, err := g.registry.ServiceAddresses(ctx, "rating")
+	if err != nil {
+		return 0, err
+	}
+	url := fmt.Sprintf("http://%s/rating", addrs[rand.Intn(len(addrs))])
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return 0, err
 	}
@@ -51,7 +58,12 @@ func (g *Gateway) GetAggregatedRating(ctx context.Context, recordID model.Record
 
 // AddRating writes a new rating.
 func (g *Gateway) AddRating(ctx context.Context, recordID model.RecordID, recordType model.RecordType, rating *model.Rating) error {
-	req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("%v/rating", g.addr), nil)
+	addrs, err := g.registry.ServiceAddresses(ctx, "rating")
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("http://%s/rating", addrs[rand.Intn(len(addrs))])
+	req, err := http.NewRequest(http.MethodPut, url, nil)
 	if err != nil {
 		return err
 	}
