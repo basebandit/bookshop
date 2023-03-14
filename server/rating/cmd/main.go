fe294522297dda25fdadfa4fc754176f6528f5ad
basebandit <evansonmwangi83@gmail.com>
2023-03-14 11:11:06 +0300

    use the consul registry for service discovery

--- server/rating/cmd/main.go
+++ server/rating/cmd/main.go
@@ -1,21 +1,54 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/basebandit/bookshop/server/pkg/discovery"
+	"github.com/basebandit/bookshop/server/pkg/discovery/consul"
 	"github.com/basebandit/bookshop/server/rating/internal/controller/rating"
 	httphandler "github.com/basebandit/bookshop/server/rating/internal/handler/http"
 	"github.com/basebandit/bookshop/server/rating/internal/repository/memory"
 )
 
+const serviceName = "rating"
+
 func main() {
-	log.Println("starting the rating service")
+	var port int
+	flag.IntVar(&port, "port", 8082, "API handler port")
+	flag.Parse()
+	log.Printf("starting the rating service on port %d\n", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Printf("failed to report healthy state: %v\n", err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer func() {
+		if err := registry.Deregister(ctx, instanceID, serviceName); err != nil {
+			panic(err)
+		}
+	}()
 	repo := memory.New()
-	ctrl := rating.New(repo)
-	h := httphandler.New(ctrl)
+	svc := rating.New(repo)
+	h := httphandler.New(svc)
 	http.Handle("/rating", http.HandlerFunc(h.Handle))
-	if err := http.ListenAndServe(":8082", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
