3ef8b4cb57e57143a1c92a89a711ffb4df646961
basebandit <evansonmwangi83@gmail.com>
2023-03-14 11:12:15 +0300

    use the consul registry for service discovery

--- server/book/cmd/main.go
+++ server/book/cmd/main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/basebandit/bookshop/server/book/internal/controller/book"
-	metadatahttp "github.com/basebandit/bookshop/server/book/internal/gateway/metadata/http"
-	ratinghttp "github.com/basebandit/bookshop/server/book/internal/gateway/rating/http"
+	metadatagateway "github.com/basebandit/bookshop/server/book/internal/gateway/metadata/http"
+	ratinggateway "github.com/basebandit/bookshop/server/book/internal/gateway/rating/http"
 	httphandler "github.com/basebandit/bookshop/server/book/internal/handler/http"
+	"github.com/basebandit/bookshop/server/pkg/discovery"
+	"github.com/basebandit/bookshop/server/pkg/discovery/consul"
 )
 
+const serviceName = "book"
+
 func main() {
-	log.Println("starting the book service")
-	metdataGateway := metadatahttp.New("localhost:8081")
-	ratingGateway := ratinghttp.New("localhost:8082")
-	ctrl := book.New(ratingGateway, metdataGateway)
-	h := httphandler.New(ctrl)
+	var port int
+	flag.IntVar(&port, "port", 8083, "API handler port")
+	flag.Parse()
+	log.Printf("starting the book service on port %d\n", port)
+	registry, err := consul.NewRegistry("localhost:8500")
+	if err != nil {
+		panic(err)
+	}
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+		panic(err)
+	}
+	go func() {
+		for {
+			if err := registry.ReportHealthyState(instanceID, serviceName); err != nil {
+				log.Printf("failed to report healthy state: %v\n", err.Error())
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	defer func() {
+		if err := registry.Deregister(ctx, instanceID, serviceName); err != nil {
+			panic(err)
+		}
+	}()
+	metdataGateway := metadatagateway.New(registry)
+	ratingGateway := ratinggateway.New(registry)
+	svc := book.New(ratingGateway, metdataGateway)
+	h := httphandler.New(svc)
 	http.Handle("/book", http.HandlerFunc(h.GetBook))
-	if err := http.ListenAndServe(":8083", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		panic(err)
 	}
 }
